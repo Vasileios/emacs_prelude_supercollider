a1194bb98db938fe385ec052a3920f0c3103bdb3
vasilis <agios.mur@gmail.com>
2020-11-11 17:06:27 +0200

    superCollider Emacs

---
+++ 001_packages.el
@@ -0,0 +1,37 @@
+;;; packages --- 2018-02-28 04:14:06 PM
+
+;;; Commentary:
+;;; this is only some of the packags.
+;;; Some other required packages are loaded in the following postload files.
+;;; using prelude-require-package ensures that the packges are loaded
+;;; at the time required, if necessary.
+
+;;; Code:
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(require 'prelude-packages)
+
+;; also load prelude-required packages manually.
+;; this loads packages which are not (auto-) loaded otherwise.
+;; apparently prelude-required packages will be available after restarting emacs twice ...
+(defun prelude-load-require-package (package)
+  "Install PACKAGE unless already installed."
+  (unless (memq package prelude-packages)
+    (add-to-list 'prelude-packages package))
+  (unless (package-installed-p package)
+    (package-install package))
+  (require package))
+
+(defun prelude-load-require-packages (packages)
+  "Ensure PACKAGES are installed.
+Missing packages are installed automatically."
+  (mapc #'prelude-load-require-package packages))
+
+(prelude-load-require-packages
+ '(
+   moe-theme
+   powerline
+   multi-term))
+
+(provide 'packages)
+;;; 001_packages.el ends here
